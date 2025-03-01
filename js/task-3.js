14a5cf0ca528df93ed9522dae873e58215b44720
Olya <obsmyk202001@gmail.com>
2025-03-01 14:49:48 +0100

    changed all

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,14 @@
 function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding) * 2;
-  const borderWidth = parseFloat(border) * 2;
-  return contentWidth + paddingWidth + borderWidth;
+  return (
+    Number.parseFloat(content) +
+    Number.parseFloat(padding) * 2 +
+    Number.parseFloat(border) * 2
+  );
 }
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+/*const contentWidth = Number.parseFloat(content);
+  const paddingWidth = Number.parseFloat(padding) * 2;
+  const borderWidth = Number.parseFloat(border) * 2;
+  return contentWidth + paddingWidth + borderWidth;*/
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
